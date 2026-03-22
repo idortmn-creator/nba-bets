64cbc6b76676094d0e83ea870a0a9d03bc67be1d
idortmn-creator <idortmn@gmail.com>
2026-03-22 15:02:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,2 @@
-const CACHE = 'nba-bets-v4';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', () => {});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
